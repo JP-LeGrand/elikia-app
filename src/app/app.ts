eaaaa48bcef3b821caf9c417372046226df64922
JP-LeGrand <55460126+JP-LeGrand@users.noreply.github.com>
2026-05-18 23:06:46 +0200

    Update dynamic meta tags and title for improved SEO and multilingual support

--- src/app/app.ts
+++ src/app/app.ts
@@ -35,9 +35,18 @@ export class App {
                 this.previousUrl = currentUrl;
 
                 // Set dynamic meta tags and title (customize per route as needed)
-                this.titleService.setTitle('Elikia French4All');
-                this.meta.updateTag({ name: 'description', content: 'Elikia French4All offers comprehensive online French courses for all levels. Learn French with expert teachers, interactive lessons, and a supportive community. Start your French learning journey today!' });
-                this.meta.updateTag({ name: 'keywords', content: 'French learning, online French courses, language school, French for all, Elikia, French classes, French lessons, French teacher, learn French online, French education, French language, French grammar, French vocabulary, French speaking, French culture, French beginners, advanced French, French tutoring, French practice, French conversation, French exam preparation' });
+                this.titleService.setTitle('Elikia French4All | Apprenez le français en ligne');
+                this.meta.updateTag({
+                    name: 'description',
+                    content: `Elikia French4All offers comprehensive online French courses for all levels. Learn French with expert teachers, interactive lessons, and a supportive community. Start your French learning journey today!
+
+                    Elikia French4All propose des cours de français en ligne pour tous les niveaux. Apprenez le français avec des professeurs expérimentés, des leçons interactives et une communauté solidaire. Commencez votre apprentissage du français dès aujourd'hui !`
+                });
+                this.meta.updateTag({
+                    name: 'keywords',
+                    content: `French learning, online French courses, language school, French for all, Elikia, French classes, French lessons, French teacher, learn French online, French education, French language, French grammar, French vocabulary, French speaking, French culture, French beginners, advanced French, French tutoring, French practice, French conversation, French exam preparation,
+                    Apprendre le français, cours de français en ligne, école de langues, français pour tous, Elikia, cours de français, leçons de français, professeur de français, apprendre le français en ligne, éducation française, langue française, grammaire française, vocabulaire français, parler français, culture française, débutants français, français avancé, tutorat français, pratique du français, conversation française, préparation aux examens de français`
+                });
             }
         });
     }
