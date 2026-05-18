70289bc8ed5b303d5144865b95e1064bd4acfc7d
JP-LeGrand <55460126+JP-LeGrand@users.noreply.github.com>
2026-05-18 20:00:09 +0200

    Refactor main banner component: remove carousel implementation and update background images for desktop and mobile

--- src/app/demos/course-portal-online/hometwo-main-banner/hometwo-main-banner.component.ts
+++ src/app/demos/course-portal-online/hometwo-main-banner/hometwo-main-banner.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { LanguageService } from '../../../common/language/language.service';
 import { inject } from '@angular/core';
 
 @Component({
     selector: 'app-hometwo-main-banner',
-    imports: [CarouselModule, RouterLink],
+	imports: [RouterLink],
     templateUrl: './hometwo-main-banner.component.html',
     styleUrls: ['./hometwo-main-banner.component.scss']
 })
@@ -14,20 +13,5 @@ export class HometwoMainBannerComponent {
 
     /** Expose service publicly so templates call lang.t('key') directly */
     readonly lang = inject(LanguageService);
-
-    homeSlides: OwlOptions = {
-		items: 1,
-		nav: true,
-		loop: true,
-		dots: true,
-		autoplay: true,
-		smartSpeed: 500,
-		autoHeight: true,
-		autoplayHoverPause: true,
-		navText: [
-			"<i class='bx bx-chevron-left'></i>",
-			"<i class='bx bx-chevron-right'></i>"
-		]
-    }
 
 }
